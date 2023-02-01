adeaea351c7b0472f051fe7fd3479125305dedf6
demonslayer07 <108910497+demonslayer07@users.noreply.github.com>
2023-02-01 13:42:22 +0800

    Add files via upload

---
+++ Hotel/payment/modal.php
@@ -0,0 +1,59 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Payment</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>RedDoorz Lower Bicutan - Online Payment </h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Scan now</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Pay via GCash and Paypal</h4>
+        </div>
+        <div class="modal-body">
+          <p>Gcash Send Money via Personal QR.</p>
+          <p>To learn sending money via Personal QR visit this GCash website.</p>
+          <br>
+          <br>
+          <img src="qrgcash.jpg" alt=" " class="img-responsive">
+            <br>
+            <br>
+            <br>
+          <p>We Accept Paypal Payments Quickly: Using QR Codes for Touch-Free Payments.</p>
+          <p>To learn sending money via Personal QR visit this Paypal website.</p>
+          <br>
+          <br>
+          <img src="qrpaypal.jpg" alt=" " class="img-responsive">
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+  
+  
+</div>
+
+</body>
+</html>
